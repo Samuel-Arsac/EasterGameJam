70f17328d6ef31ce53b40bac8f11fd7647d887f3
AlexLoup33 <a.loupoueyou@icloud.com>
2025-03-29 16:56:33 +0100

    Update: class vegetable

--- Assets/scripts/Cultures/Patate.cs
+++ Assets/scripts/Cultures/Patate.cs
@@ -1,16 +1,77 @@
+using System;
 using UnityEngine;
 
-public class Patate : MonoBehaviour
+public class Patate : MonoBehaviour, IFarmable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+	public GameObject patate;
+	private const string type = "patate";
+	private int state;
+	private int level;
+    private int collectNumber;
+    private const double growTime = 5;
+	private double timer;
+
+    public string getType()
     {
-        
+        return TYPE;
     }
+
+    public int getState()
+    {
+        return state;
+    }
+
+    public void upgrade()
+    {
+        return; 
+    }
+
+    public int collect()
+    {
+        state = 0;
+		timer = 0;
+		return collectNumber;
+    }
+
+    public int getCollectNumber()
+    {
+        return collectNumber;
+    }
+
+    public int getLevel()
+    {
+        return level;
+    }
+
+	public Patate(int st, int lv)
+	{
+		state = st;
+		level = lv;
+
+		//Calcul of the timer
+		if (state != 2){
+			if (state == 0)
+				timer = (double)0;
+			else
+				timer = growTime/2;
+		}
+
+		//Calcul of the collectNumber
+		collectNumber = (int)Math.Ceiling(Math.Pow(level, 2));
+	}
 
     // Update is called once per frame
     void Update()
     {
-        
+		if (state != 2){
+			timer += Time.deltaTime;
+
+			if (timer >= growTime && state == 1)
+				state = 2;
+
+			if (timer >= (growTime/2) && state == 0)
+				state = 1;
+		}
     }
+
 }
